31b3b42284f80af058b22d56790ebe61c063f7ac
Diana <diana.begun@mail.ru>
2023-06-01 14:36:13 +0300

    Repush practices

--- lab18/src/main/java/com/example/pw18/authentication/cookies/CookieRepo.java
+++
@@ -1,8 +0,0 @@
-package com.example.pw18.authentication.cookies;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CookieRepo extends JpaRepository<CookieEntity, Long> {
-}
--- lab18/src/main/java/com/example/pw18/phones/PhoneRepo.java
+++
@@ -1,8 +0,0 @@
-package com.example.pw18.phones;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PhoneRepo extends JpaRepository<Phone, Long> {
-}
